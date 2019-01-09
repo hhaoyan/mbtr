cc073ae96ee2c6aa67c17c02a0d959700072e2ad
Haoyan Huo <hhaoyann@gmail.com>
2019-01-09 11:57:05 -0800

    add visualize scripts.

--- examples/visualize.py
+++ examples/visualize.py
@@ -1,40 +1,15 @@
-import itertools
 import os
 
 import matplotlib.pyplot as plt
 import numpy
-
 from mbtr import read_xyz_molecule, MolsMBTR2DIQuadW, read_xyz_crystal, PeriodicMBTR3D
 
-ELEMENTS = [
-    'H', 'He',
-    'Li', 'Be', 'B', 'C', 'N', 'O', 'F', 'Ne',
-    'Na', 'Mg', 'Al', 'Si', 'P', 'S', 'Cl', 'Ar',
-    'K', 'Ca', 'Sc', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Cu',
-    'Zn', 'Ga', 'Ge', 'As', 'Se', 'Br', 'Kr',
-    'Rb', 'Sr', 'Y', 'Zr', 'Nb', 'Mo', 'Tc', 'Ru', 'Rh', 'Pd', 'Ag',
-    'Cd', 'In', 'Sn', 'Sb', 'Te', 'I', 'Xe',
-    'Cs', 'Ba', 'La', 'Ce', 'Pr', 'Nd', 'Pm', 'Sm', 'Eu', 'Gd', 'Tb',
-    'Dy', 'Ho', 'Er', 'Tm', 'Yb', 'Lu', 'Hf', 'Ta', 'W', 'Re', 'Os',
-    'Ir', 'Pt', 'Au', 'Hg', 'Tl', 'Pb', 'Bi', 'Po', 'At', 'Rn',
-    'Fr', 'Ra', 'Ac', 'Th', 'Pa', 'U', 'Np', 'Pu', 'Am', 'Cm', 'Bk',
-    'Cf', 'Es', 'Fm', 'Md', 'No', 'Lr', 'Rf', 'Db', 'Sg', 'Bh', 'Hs',
-    'Mt', 'Ds', 'Rg', 'Cn', 'Nh', 'Fl', 'Mc', 'Lv', 'Ts', 'Og'
-]
 
-
-def visualize_mbtr(array, an, x_range, rank, title='Visualize'):
+def visualize_mbtr(array, lines, x_range, title='Visualize'):
     fig = plt.figure()
 
-    # for atom_tuple in itertools.combinations_with_replacement(an, rank):
-    for atom_tuple in itertools.product(an, repeat=rank):
-        index = list(an.index(x) for x in atom_tuple)
-        index.append(slice(0, array.shape[-1]))
-        values = array[index]
-
-        atoms = tuple(ELEMENTS[i - 1] for i in atom_tuple)
-        atoms_name = ''.join(atoms)
-        plt.plot(x_range, values, label=atoms_name)
+    for label, index in lines:
+        plt.plot(x_range, array[index], label=label)
 
     plt.xlabel('Geometry function')
     plt.ylabel('Density')
@@ -53,13 +28,25 @@ def visualize_aspirin():
     )
     molecules = read_xyz_molecule(aspirin_fn)
 
-    mbtr = MolsMBTR2DIQuadW(grid_size=200, smearing_factor=0.023)
+    mbtr = MolsMBTR2DIQuadW(grid_size=500, smearing_factor=0.023)
     mbtr.fit(molecules)
-    tensor_range = numpy.linspace(mbtr.tensor_range[0], mbtr.tensor_range[1], 200)
+    tensor_range = numpy.linspace(mbtr.tensor_range[0], mbtr.tensor_range[1], 500)
 
     arrays, ans = mbtr.transform(molecules)
     for array in arrays:
-        visualize_mbtr(array, ans, tensor_range, 2, title='Aspirin')
+        visualize_mbtr(
+            array.reshape((-1, 500)),
+            (
+                ('HH', 0),
+                ('HC, CH', 1),
+                ('HO, OH', 2),
+                ('CC', 4),
+                ('CO, OC', 5),
+                ('OO', 8),
+            ),
+            tensor_range,
+            title='Aspirin'
+        )
 
 
 def visualize_nacl():
@@ -72,15 +59,23 @@ def visualize_nacl():
     )
     nacl = read_xyz_crystal(nacl_fn)
 
-    mbtr = PeriodicMBTR3D(grid_size=200, smearing_factor=0.03, d=3.0)
+    mbtr = PeriodicMBTR3D(grid_size=500, smearing_factor=0.05, d=2.0)
     mbtr.fit(nacl)
-    tensor_range = numpy.linspace(mbtr.tensor_range[0], mbtr.tensor_range[1], 200)
+    tensor_range = numpy.linspace(mbtr.tensor_range[0], mbtr.tensor_range[1], 500)
 
     arrays, ans = mbtr.transform(nacl)
     for array in arrays:
-        visualize_mbtr(array, ans, tensor_range, 3, title='NaCl')
+        visualize_mbtr(
+            array.reshape((-1, 500)), (
+                ('NaNaNa, ClClCl', 0),
+                ('NaClCl, ClClNa, ClNaNa, NaNaCl', 1),
+                ('NaClNa, ClNaCl', 2),
+            ),
+            tensor_range,
+            title='NaCl'
+        )
 
 
 visualize_aspirin()
-# visualize_nacl()
+visualize_nacl()
 plt.show()
